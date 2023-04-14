f1009bf94cf9f5aebd851771a6017420bd2d8596
vip-prjpti <prajapativipul0408@gmail.com>
2023-04-14 16:19:03 +0530

    added ratings and title

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import "./row.css";
-import Youtube from 'react-youtube';
+import Youtube from "react-youtube";
 import movieTrailer from "movie-trailer";
 
 const base_URL = "https://image.tmdb.org/t/p/original/";
+
+
 
 const Row = (props) => {
   const { title, fetchUrl, isLargeRow } = props;
@@ -12,10 +14,12 @@ const Row = (props) => {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
 
+ 
+  
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      // console.log(request.data.results);
+      console.log(request.data.results);
       setMovies(request.data.results);
       return request;
     }
@@ -23,42 +27,53 @@ const Row = (props) => {
   }, [fetchUrl]);
 
   const opts = {
-    height: '400',
-    width: '100%',
+    height: "400",
+    width: "100%",
     playerVars: {
       autoplay: 1,
     },
   };
 
-const handleClick = (movie) => {
-  if(trailerUrl) {
-    setTrailerUrl('');
-  }else{
-    movieTrailer(movie?.name || movie?.title || " " )  
-    .then((url) => { 
-      const urlParams = new URLSearchParams(new URL(url).search);
-      setTrailerUrl(urlParams.get("v"));
-    })
-    .catch((error) => console.log(error));
-  }
-}
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || " ")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
+    <>
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
-          <img
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-            alt={movie.name}
-          />
+          <div>
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+            {/* <Vote movie = {movie}/> */}
+            <p className="movie__votes">{movie.vote_average}</p>
+            <p>{movie.title}</p>
+            </div>
         ))}
       </div>
-      { trailerUrl && <Youtube videoId={trailerUrl} opts={opts}></Youtube>}
+
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}></Youtube>}
     </div>
+    </>
   );
 };
 
